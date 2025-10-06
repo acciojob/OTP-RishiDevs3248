51e52eeb6795fc9f487afa718ce1838b2964863c
Hrishikesh Alabnur <144805491+RishiDevs3248@users.noreply.github.com>
2025-10-06 10:08:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let ips = document.querySelectorAll(".code")
+ips[0].focus();
+ips.forEach((ip,index)=>{
+	ip.addEventListener('input',(e)=>{
+		if(e.target.value.length == 1 && index < ips.length-1){
+			ips[index+1].focus();
+		}
+	})
+	ip.addEventListener('keydown',(e)=>{
+		if (e.key === 'Backspace') {
+			if (e.target.value.length == 0 && index > 0) {
+				ips[index-1].value = ""
+				ips[index-1].focus();
+			}else{
+				ips[index].value = ""
+			}
+		}
+	})
+})
+
+
